71b57be7b7eabca8d69068d30bfbaf565f1fd714
Artur Pazurkiewicz <pazurkiewiczartur@gmail.com>
2019-12-11 14:53:04 +0100

    added pass button

--- src/main/java/JADevelopmentTeam/client/ClientGui.java
+++ src/main/java/JADevelopmentTeam/client/ClientGui.java
@@ -10,26 +10,35 @@ public class ClientGui extends JFrame {
     private Board board;
     private JMenuBar menuBar;
     private JLabel label;
-    public ClientGui(int size){
+    private JButton passButton;
+
+    public ClientGui(int size) {
         super();
         menuBar = new JMenuBar();
         menuBar.setBorderPainted(true);
+        menuBar.setLayout(new BorderLayout());
         label = new JLabel("Unknown color");
         label.setOpaque(true);
-        menuBar.add(label);
+        passButton = new JButton("PASS");
+        passButton.setBackground(Color.RED);
+        passButton.addActionListener(e -> ServerConnector.getInstance().sendData(new DataPackage("Pass", DataPackage.Info.Pass)));
+        menuBar.add(label,BorderLayout.LINE_START);
+        menuBar.add(passButton,BorderLayout.LINE_END);
+        passButton.setVisible(false);
         this.setJMenuBar(menuBar);
         this.setSize(500, 500);
         board = new Board(size);
         this.add(board);
         this.setVisible(true);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setBackground(new Color(224,172,105));
+        this.setBackground(new Color(224, 172, 105));
     }
+
     public void handleInput(DataPackage dataPackage) {
         System.out.println("Handle");
         switch (dataPackage.getInfo()) {
             case StoneTable: {
-                board.setIntersections((Intersection[][]) dataPackage.getData(),this);
+                board.setIntersections((Intersection[][]) dataPackage.getData(), this);
                 for (int i = 0; i < 9; i++) {
                     for (int j = 0; j < 9; j++) {
                         System.out.println(board.getIntersections()[j][i].exist() + " " + j + " " + i);
@@ -39,14 +48,14 @@ public class ClientGui extends JFrame {
                 break;
             }
             case Info:
-                JOptionPane.showMessageDialog(this, dataPackage.getData(),"WARNING",JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(this, dataPackage.getData(), "WARNING", JOptionPane.ERROR_MESSAGE);
                 break;
             case Turn:
                 this.setTitle((String) dataPackage.getData());
                 break;
             case PlayerColor:
                 String color = (String) dataPackage.getData();
-                if (color.equals("black")){
+                if (color.equals("black")) {
                     label.setText("You are playing black stones");
                     label.setForeground(Color.WHITE);
                     label.setBackground(Color.BLACK);
@@ -55,6 +64,7 @@ public class ClientGui extends JFrame {
                     label.setForeground(Color.BLACK);
                     label.setBackground(Color.WHITE);
                 }
+                passButton.setVisible(true);
         }
     }
 }
